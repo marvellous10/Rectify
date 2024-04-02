e691518d5806ff671b7c5f6d3be22a263c7979b8
marvellous10 <nwachukwumarvelous90@gmail.com>
2024-04-02 23:54:56 +0100

    made corrections

--- Rectify-frontend/layouts/default.vue
+++ Rectify-frontend/layouts/default.vue
@@ -1,11 +1,5 @@
-<script lang="ts">
-import { SpeedInsights } from '@vercel/speed-insights/vue';
-</script>
-
-
 <template>
     <div class="pagelayout">
-        <SpeedInsights />
         <nav class="navbar">
             <NuxtLink to="/" class="logo-placeholder">
                 <div class="logo">
