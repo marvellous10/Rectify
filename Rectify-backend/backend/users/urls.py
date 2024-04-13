3be07e064a6b55d0906f7554aaac2b9120afdaee
marvellous10 <nwachukwumarvelous90@gmail.com>
2024-04-13 15:09:09 +0100

    backend changes and fixed azure deploy

--- Rectify-backend/backend/users/urls.py
+++ Rectify-backend/backend/users/urls.py
@@ -4,7 +4,7 @@ from .views import *
 urlpatterns = [
     path('spotify/login/', SpotifyLoginView.as_view(), name='spotify_login'),
     path('spotify/callback/', SpotifyCallBackView.as_view(), name='spotify_callback'),
-    path('spotify/testcode/', GetCodeQuery.as_view(), name='spotify_getcode'), #For debug, edit the view code
+    #path('spotify/testcode/', GetCodeQuery.as_view(), name='spotify_getcode'), #For debug, edit the view code
     path('spotify/deletesession/', DeleteSession.as_view(), name='spotify_deletesession'), #For debug, edit the view code
     path('spotify/getsession/', GetSessions.as_view(), name='spotify_getsession'), #For debug, edit the view code
 ]
